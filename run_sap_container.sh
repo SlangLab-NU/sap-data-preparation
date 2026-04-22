71e5530180afa81e8bf207ebc1ee9004d9a65fb3
jordan-git-lewis <jlgitrepo@gmail.com>
2026-04-22 11:31:14 -0400

    updated container to create cache within working dir instead of users home dir

--- run_sap_container.sh
+++ run_sap_container.sh
@@ -5,7 +5,25 @@
 export PYTHONPATH=""
 export apptainer_image=sap_data_prep.sif
 
+export CACHE_ROOT=/scratch/lewis.jor/cache
+
+export APPTAINERENV_XDG_CACHE_HOME=$CACHE_ROOT
+
+export APPTAINERENV_NLTK_DATA=$CACHE_ROOT/nltk
+export APPTAINERENV_HF_HOME=$CACHE_ROOT/huggingface
+export APPTAINERENV_TRANSFORMERS_CACHE=$CACHE_ROOT/huggingface
+export APPTAINERENV_MPLCONFIGDIR=$CACHE_ROOT/matplotlib
+export APPTAINERENV_TORCH_HOME=$CACHE_ROOT/torch
+export APPTAINERENV_CACHED_PATH_CACHE_ROOT=$CACHE_ROOT/cached_path
+
+mkdir -p $CACHE_ROOT/nltk
+mkdir -p $CACHE_ROOT/huggingface
+mkdir -p $CACHE_ROOT/matplotlib
+mkdir -p $CACHE_ROOT/torch
+mkdir -p $CACHE_ROOT/cached_path
+
 apptainer shell --nv \
-        --bind /scratch/lewis.jor:/scratch/lewis.jor \
+        --no-home \
+       	--bind /scratch/lewis.jor:/scratch/lewis.jor \
        	$apptainer_image
 
